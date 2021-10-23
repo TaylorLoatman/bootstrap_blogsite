8685a5c8fd70999b7de16ebb70c2b3a238c8df4b
Taylor Loatman <taylor.loatman@icloud.com>
2021-10-23 09:56:12 -0400

    Contact email enabled

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
-from pprint import pprint
+import smtplib
 
 app = Flask(__name__)
-
-# pprint(all_post)
 all_post = requests.get(url='https://api.npoint.io/d30c480322a8d2311384').json()
+
 
 @app.route('/')
 def get_home():
@@ -25,6 +24,27 @@ def get_post(blog_id):
 @app.route('/contact')
 def get_contact():
     return render_template('contact.html')
+
+
+@app.route('/form-entry', methods=['POST'])
+def receive_data():
+    name = request.form['user']
+    email = request.form['email']
+    phone = request.form['phone']
+    message = request.form['message']
+
+    my_email = "tloatmancodes@gmail.com"
+    password = 'Peyton030%'
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="tloatman77@yahoo.com",
+            msg=f"Subject:New Message from {name}\n\nEmail: {email}\nNumber: {phone}\nMessage: {message}"
+            )
+    return render_template('form_entry.html')
 
 
 if __name__ == "__main__":
